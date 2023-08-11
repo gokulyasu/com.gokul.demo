0e531303f2a0c0e16857d85108215bf1f8adeaf1
mm0690 <iamgokul381@gmail.com>
2023-08-11 13:26:17 +0530

    darkMode

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,14 +1,33 @@
 sap.ui.define(
   ["./BaseController",
-   "sap/m/MessageBox"
+   "sap/m/MessageBox",
+   "sap/ui/core/Configuration"
 ],
-  function (BaseController, MessageBox) {
+  function (BaseController, MessageBox,Configuration) {
     "use strict";
 
     return BaseController.extend("com.gokul.demo.controller.Main", {
+      onInit: function() {
+        // Get the Configuration object
+        var oConfiguration = new Configuration();
+  
+        
+        // Apply the theme change to the entire application
+        sap.ui.getCore().applyTheme("sap_horizon");
+      },
       sayHello: function () {
         MessageBox.show("Hello GokulJanani!");
       },
+      changeTheme:function(oEvent){
+        if(oEvent.getParameter("state")){
+          Configuration.setTheme("sap_horizon_dark");
+        }else {
+          Configuration.setTheme("sap_horizon");
+        }
+
+
+        
+      }
     });
   }
 );
